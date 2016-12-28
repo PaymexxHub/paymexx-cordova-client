b046d42f429751f31313c297af782a4950687f03
PaymexxHub <developer@zercomsystems.com>
2016-12-28 21:27:43 +0100

    Updating app.js
    Cleaning up api test parameters

--- Paymexx/www/js/app.js
+++ Paymexx/www/js/app.js
@@ -6,11 +6,11 @@ app.onLoad = function() {
 	document.addEventListener('deviceready', app.onDeviceReady, false);
 	document.addEventListener("deviceready", app.initEvents, false) ;
 },
-app.MerchantID="MUA-57D5F16592CFF",
-app.ApiKey = "zsec-db5266fc0e80f7557c502abb19643ff9", // Your secert key provided by Paymexx. Can be found on the dahsboard
-app.ProductName = "Baby Storefront", // The Product name. This should already be created in the Paymexx Dashboard.
+app.MerchantID="",
+app.ApiKey = "", // Your secert key provided by Paymexx. Can be found on the dahsboard
+app.ProductName = "", // The Product name. This should already be created in the Paymexx Dashboard.
 app.Env = "test", // The environment you wish to run a transaction in ( either Test mode or Live mode)
-app.GatewayUrl = 'http://paymexx.com/gateway/mobile',
+app.GatewayUrl = 'https://paymexx.com/gateway/mobile',
 app.onAppReady = function(){
 	if( navigator.splashscreen && navigator.splashscreen.hide ) {   // Cordova API detected
 		   navigator.splashscreen.hide() ; 
@@ -52,10 +52,9 @@ app.launchPaymexxGateway =  function() {
                     '&amount='+ frmdata.amount + '&currency='+ frmdata.currency + '&response_url=' +frmdata.response_url; 
     try {
         var payRef = window.open(paymentUrl, 'gWin', 'location=yes');
-                // $("#win").val('gWin');
+               
     } catch (e) {
-       // console.log(fName, "catch, failure");
-       alert('Gateway failed to launch. Kindly resolve this issue or contact the Paymexx Adminstrator!');
+       alert('Gateway failed to launch. Kindly resolve this issue or contact the Paymexx Administrator!');
     }
 
 };
